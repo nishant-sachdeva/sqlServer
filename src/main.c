574c17240becb1800c99ccc7f3f40d41d8ae075e
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2022-03-04 12:36:01 +0530

    Started Database Building. REPL skeleton has been completed

---
+++ src/main.c
@@ -0,0 +1,54 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char *buffer;
+    size_t bufferLength;
+    ssize_t inputLength;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() {
+    InputBuffer *inputBuffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    inputBuffer->buffer = NULL;
+    inputBuffer->bufferLength = 0;
+    inputBuffer->inputLength = 0;
+
+    return inputBuffer;
+}
+
+void print_prompt() { printf("db > "); }
+
+void read_input(InputBuffer *inputBuffer) {
+    ssize_t bytesRead = getline(&(inputBuffer->buffer), &(inputBuffer->bufferLength), stdin);
+
+    if (bytesRead <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    inputBuffer->inputLength = bytesRead - 1;
+    inputBuffer->buffer[bytesRead - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer *inputBuffer) {
+    free(inputBuffer->buffer);
+    free(inputBuffer);
+}
+
+int main(int argc, char **argv) {
+    InputBuffer* inputBuffer = new_input_buffer();
+
+    while (true) {
+        print_prompt();
+        read_input(inputBuffer);
+
+        if (strcmp(inputBuffer->buffer, ".exit") == 0) {
+            close_input_buffer(inputBuffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'\n", inputBuffer->buffer);
+        }
+    }
+}
